a0ced4730c75fd217ec313b09f0fa8ab5b8b7b20
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2025-04-10 23:02:26 +0100

    :construction: added mean activity subtraction funcion.

--- foundations/2012_alexnet/alexnet_ILSVRC2012.py
+++ foundations/2012_alexnet/alexnet_ILSVRC2012.py
@@ -7,6 +7,19 @@ import torchvision.transforms as transforms
 from tqdm import tqdm
 
 
+mean_activity_of_pixels = None
+
+def SubtractMeanPixelActivity(x: np.array):
+    """
+    A method that subracts precomputed mean activity of pixels from each pixel.
+    The mean activity of pixels are computed using the entire training set.
+
+    Parameters
+    ----------
+
+    """
+    return x - mean_activity_of_pixels
+
 def ToTensorNoScaling(x: np.array) -> torch.Tensor:
     """
     A method for coverting RGB image in numpy format into torch Tensor without 0-1 scaling.
@@ -105,6 +118,8 @@ def get_train_data_loader(train_dataset_path: str) -> torch.utils.data.DataLoade
             transforms.Resize(256),
             transforms.CenterCrop(227),
             transforms.Lambda(ToTensorNoScaling),
+            transforms.Lambda(SubtractMeanPixelActivity),
+            transforms.CenterCrop(227),
         ]
     )
 
@@ -116,7 +131,7 @@ def get_train_data_loader(train_dataset_path: str) -> torch.utils.data.DataLoade
     train_loader = torch.utils.data.DataLoader(
         trainset,
         shuffle=False,
-        num_workers=4,
+        num_workers=1,
         drop_last=True,
         batch_size=256,
         persistent_workers=True,
@@ -129,7 +144,22 @@ def get_train_data_loader(train_dataset_path: str) -> torch.utils.data.DataLoade
 if __name__ == "__main__":
 
     train_dataset_path = "/home/sree/data/ILSVRC2012"
-    mean_pixels = compute_mean_pixels(train_dataset_path)
+    compute_mean_activity = False
 
-    mean_pixels = mean_pixels.numpy()
-    np.save("mean_pixels.npy", mean_pixels)
+    if compute_mean_activity is True:
+        mean_pixels = compute_mean_pixels(train_dataset_path)
+
+        # assign the global variable defined at the top
+        mean_activity_of_pixels = mean_pixels.numpy()
+        np.save("mean_activity_of_pixels.npy", mean_activity_of_pixels)
+    else:
+        mean_activity_of_pixels = np.float32(np.load('/home/sree/github/llm-gym/foundations/2012_alexnet/mean_activity_of_pixels.npy'))
+        logging.debug(f"Shape of the mean activity of pixels loaded is {mean_activity_of_pixels.shape}")
+
+    train_data_loader = get_train_data_loader(train_dataset_path)
+
+    dataiter = iter(train_data_loader)
+    images, labels = next(train_data_loader)
+
+
+    
